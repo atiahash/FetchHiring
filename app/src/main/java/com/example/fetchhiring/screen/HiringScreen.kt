b5b3c5cfc44b3b4d6677bf3fe15dcdeea245e383
Atia Hashimi <ahashimi16@gmail.com>
2025-03-22 00:17:55 -0400

    display name items horizontally in a row

--- app/src/main/java/com/example/fetchhiring/screen/HiringScreen.kt
+++ app/src/main/java/com/example/fetchhiring/screen/HiringScreen.kt
@@ -1,31 +1,37 @@
 package com.example.fetchhiring.screen
 
-import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.fetchhiring.HiringViewModel
+import com.example.fetchhiring.R
+import com.example.fetchhiring.ui.theme.Typography
 
 @Composable
 fun HiringListScreen(viewModel: HiringViewModel, modifier: Modifier = Modifier) {
     val list = viewModel.hiringListState.collectAsStateWithLifecycle()
-
     LazyColumn {
         items(list.value) { hire ->
             Text(
-                text = "Group ${hire.listId}",
-                modifier = Modifier.padding(16.dp),
+                text = stringResource(R.string.group, hire.listId),
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp, end = 16.dp, start = 16.dp),
+                style = Typography.titleMedium
             )
             HireItems(names = hire.names)
-            Divider(color = Color.LightGray, thickness = 1.dp)
+            // Divider(color = Color.LightGray, thickness = 1.dp)
         }
     }
 }
@@ -34,11 +40,16 @@ fun HiringListScreen(viewModel: HiringViewModel, modifier: Modifier = Modifier) 
 fun HireItems(names: List<String?>) {
     LazyRow {
         items(names) { name ->
-            Text(
-                text = "$name",
-                modifier = Modifier.padding(16.dp),
-            )
+            Box(modifier = Modifier
+                .padding(4.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(Color.Gray)) {
+                Text(
+                    text = "$name",
+                    modifier = Modifier.padding(16.dp))
+            }
         }
     }
 }
+
 
