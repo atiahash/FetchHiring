9ca759ee83a46171dcfe1058724f75cf3a490eb8
Atia Hashimi <ahashimi16@gmail.com>
2025-03-23 15:25:57 -0400

    add network connectivity interceptor

--- app/src/main/java/com/example/fetchhiring/FetchApplication.kt
+++ app/src/main/java/com/example/fetchhiring/FetchApplication.kt
@@ -3,9 +3,8 @@ package com.example.fetchhiring
 import android.app.Application
 
 class FetchApplication: Application() {
-    lateinit var appContainer: HireContainer
     override fun onCreate() {
         super.onCreate()
-        appContainer = HireContainer()
+        HireContainer(applicationContext)
     }
 }
--- app/src/main/java/com/example/fetchhiring/HireContainer.kt
+++ app/src/main/java/com/example/fetchhiring/HireContainer.kt
@@ -1,22 +1,25 @@
 package com.example.fetchhiring
 
+import android.content.Context
 import com.example.fetchhiring.data.HireRepository
 import com.example.fetchhiring.data.HireApiService
 import com.example.fetchhiring.data.HireApiClient
+import com.example.fetchhiring.data.NetworkConnectivityInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.converter.gson.GsonConverterFactory
 
-class HireContainer {
+class HireContainer(context: Context) {
 
     init {
-        initializeHireApiClient()
+        initializeHireApiClient(context)
 
         val hiringServiceClient = HireApiClient.hireApiService.create(HireApiService::class.java)
         HireRepository.inject(hiringServiceClient)
     }
 
-    private fun initializeHireApiClient() {
+    private fun initializeHireApiClient(context: Context) {
+        val networkInterceptor = NetworkConnectivityInterceptor(context)
         val httpLoggingInterceptor = if (!isProdBuild()) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         } else {
@@ -26,6 +29,7 @@ class HireContainer {
         val okHttpClient: OkHttpClient.Builder =
             OkHttpClient().newBuilder()
                 .addInterceptor(httpLoggingInterceptor)
+                .addInterceptor(networkInterceptor)
 
         HireApiClient.inject(
             okHttpClient = okHttpClient.build(),
